65e153195ef68bf51427a161216f94d6fd5a523f
MasatakaIshii <masaradio78@gmail.com>
2020-11-01 17:38:08 +0100

    Create FileReader and the readFile method

---
+++ src/utils/FileReader.cpp
@@ -0,0 +1,34 @@
+//
+// Created by masat on 01/11/2020.
+//
+
+#include "FileReader.h"
+#include <fstream>
+#include <utility>
+
+using namespace std;
+
+FileReader::FileReader(std::string filePath): filePath(std::move(filePath)) {
+}
+FileReader::~FileReader() {
+    filePath.erase();
+}
+
+string FileReader::readFile() const {
+    string result;
+    ifstream buffer(filePath);
+    if (buffer) {
+        buffer.seekg(0, std::ios::end);
+        result.reserve(buffer.tellg());
+        buffer.seekg(0, std::ios::beg);
+        result.assign(std::istreambuf_iterator<char>(buffer), std::istreambuf_iterator<char>());
+
+        buffer.close();
+    } else {
+        cerr << "Problem open '" + filePath + "' file";
+    }
+
+    return result;
+}
+
+
