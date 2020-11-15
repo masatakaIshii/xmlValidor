9bdd2163807b4303fdfe948f016293afda4ced67
Gwendal <gwendou95@hotmail.fr>
2020-11-15 18:22:55 +0100

    WIP xml parser

--- src/infrastructures/services/XmlParserService.cpp
+++ src/infrastructures/services/XmlParserService.cpp
@@ -2,12 +2,32 @@
 // Created by Mikadows on 09/11/2020.
 //
 
+#include <iostream>
 #include "XmlParserService.h"
 
 std::vector<ElementXml> XmlParserService::xmlParser(std::string &fileContent) {
     std::vector<ElementXml> elements = std::vector<ElementXml>();
-    std::cout << fileContent << std::endl;
+    std::string elementName;
+    std::string buffer;
 
+
+    int i = 0;
+    int y = 0;
+    while (fileContent[i] != '\0' && i < fileContent.size()) {
+//        std::cout << fileContent[i];
+//        i++;
+        if (fileContent[i] == '<') {
+            while (fileContent[y] != ' ') {
+                if (fileContent[y] == '?') break;
+                std::cout << fileContent[y] << std::endl;
+                y++;
+            }
+            i = y;
+        }
+
+        std::cout << fileContent[i];
+        i++;
+    }
 
     return elements;
 }
